04a827d030cc266797ce81486ed7c436d6166853
JeyM <jeymson9000@gmail.com>
2023-03-07 20:05:30 +0800

    the drop shadow outline for the icons come back here if the idea doesnt work

--- src/components/homepage/Homepage-Button.js
+++ src/components/homepage/Homepage-Button.js
@@ -1,9 +1,11 @@
 function HomepageButton(props) {
   return (
     <div className={`homepage-button ${props.item}`}>
-      {/* <div className="outline">Stuff</div> */}
+      <div className="outline">
         <figure className="home-category__logo"></figure>
+        </div>
       <h1>{props.label}</h1>
+      <h1 style={{color:"red"}}>Text</h1>
     </div>
   );
 }
