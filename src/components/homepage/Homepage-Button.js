2cc76b79d2d4dc0678163e60065f326382c4f67b
JeyM <jeymson9000@gmail.com>
2023-03-07 21:15:42 +0800

    make home mixins reusable

--- src/components/homepage/Homepage-Button.js
+++ src/components/homepage/Homepage-Button.js
@@ -2,10 +2,9 @@ function HomepageButton(props) {
   return (
     <div className={`homepage-button ${props.item}`}>
       {/* <div className="outline">Stuff</div> */}
-        <figure className="home-category__logo"></figure>
-      <h1>{props.label}</h1>
-      <figure className="home-category__logo home-category__logo-2"></figure>
-      <h1 style={{color:"red"}}>Text</h1>
+        <figure className={`${props.item}__logo`}></figure>
+      <h2 className="home-label">{props.label}</h2>
+      <figure className={`${props.item}__logo-outline`}></figure>
     </div>
   );
 }
