e65506930d22d5203e5449a000b6baee2163e17d
JeyM <jeymson9000@gmail.com>
2023-03-08 22:18:48 +0800

    Finished menu

--- src/components/navbar/Nav-Menu.js
+++ src/components/navbar/Nav-Menu.js
@@ -8,21 +8,21 @@ function NavMenu(props){
     const dropIn = {
         hidden: {
           x: "-100vw",
-          opacity: 0,
+          opacity: 1,
         },
         visible: {
           x: "0",
           opacity: 1,
           transition: {
             duration: 0.1,
-            type: "spring",
-            damping: 25,
-            stiffness: 500,
           },
         },
         exit: {
           x: "-100vw",
-          opacity: 0,
+          opacity: 1,
+          transition: {
+            duration: 0.1,
+          },
         },
       };
 
