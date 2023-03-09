a2d4935bef73058537ebf12dcd85bb502ab05666
JeyM <jeymson9000@gmail.com>
2023-03-09 13:55:27 +0800

    Was trying Nextjs Link, going to try router

--- src/components/homepage/Homepage-Button.js
+++ src/components/homepage/Homepage-Button.js
@@ -1,10 +1,17 @@
+import Link from "next/link";
+import { Fragment } from "react";
+import { useLocation, useSearchParams } from "react-router-dom";
+
+
 function HomepageButton(props) {
-  return (
-    <div className={`homepage-button ${props.item}`}>
+  // location = useLocation().search
+  return (<Fragment>
+    <Link className={`homepage-button ${props.item}`} href="/categories">
         <figure className={`${props.item}__logo`}></figure>
       <h2 className="home-label heading-secondary">{props.label}</h2>
       <figure className={`${props.item}__logo-outline`}></figure>
-    </div>
+    </Link>
+    </Fragment>
   );
 }
 export default HomepageButton;
--- src/pages/[givenid]/index.js
+++ src/pages/[givenid]/index.js
@@ -1,5 +1,35 @@
-function Page2(){
-    return <h1>test</h1>
+import { Fragment } from "react"
+import HomepageButton from "../../components/homepage/Homepage-Button"
+import Link from "next/link"
+
+function HomePage(){
+    return <Fragment>
+        <h1 className="heading-primary">Dashboard</h1>
+        <main className="maincontainer">
+            <HomepageButton item="home-category" label="Categories & Products"></HomepageButton>
+            <HomepageButton item="home-ongoing" label="Ongoing Sales"></HomepageButton>
+            <HomepageButton item="home-manage" label="My Mart"></HomepageButton>
+            <HomepageButton item="home-insights" label="Mart Analytics"></HomepageButton>
+            <HomepageButton item="home-receipt" label="Customer Records"></HomepageButton>
+            <HomepageButton item="home-brush" label="Mart Design"></HomepageButton>
+            <HomepageButton item="home-quiz" label="Frequently Asked Questions"></HomepageButton>
+            <HomepageButton item="home-policy" label="Terms & Policies"></HomepageButton>
+            <HomepageButton item="home-support" label="Customer Service"></HomepageButton>
+            <HomepageButton item="home-power" label="Close or Open Mart"></HomepageButton>
+        </main>
+    </Fragment>
 }
 
-export default Page2
+
+// <div className="item1"><h1>1</h1></div>
+//             <div className="item2"><h1>2</h1></div>
+//             <div className="item3"><h1>3</h1></div>
+//             <div className="item4"><h1>4</h1></div>
+//             <div className="item5"><h1>5</h1></div>
+//             <div className="item6"><h1>6</h1></div>
+//             <div className="item7"><h1>7</h1></div>
+//             <div className="item8"><h1>8</h1></div>
+//             <div className="item9"><h1>9</h1></div>
+//             <div className="item10"><h1>10</h1></div>
+
+export default HomePage
