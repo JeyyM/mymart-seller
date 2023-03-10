31f7300811bcd48ba982a1fd75cbce0f29d58724
JeyM <jeymson9000@gmail.com>
2023-03-10 13:52:10 +0800

    about to try something with the routing

--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -1,10 +1,20 @@
 import Link from "next/link"
+import { useRouter } from "next/router"
 
 function Category(props){
     const {categoryName, categoryImage, categoryDescription} = props.items
+    const router = useRouter()
+    const shopId = router.query.givenid
 
     return <Link className="category" href="#">
+    
         <img src={categoryImage} className="category-img"></img>
+        <h2 className="heading-secondary">{categoryName}</h2>
+        <h1>{categoryDescription}</h1>
+        <h1>{categoryDescription}</h1>
+        <h1>{categoryDescription}</h1>
+        <h1>{categoryDescription}</h1>
+        <h1>{categoryDescription}</h1>
         <h1>{categoryDescription}</h1>
     </Link>
 }
