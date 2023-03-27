bb22b08dde9fe6a6b67c333299318bf165e93c6f
JeyM <jeymson9000@gmail.com>
2023-03-27 15:37:19 +0800

    Back to usual Nav Setup

--- src/components/navbar/Nav-Menu-Item.js
+++ src/components/navbar/Nav-Menu-Item.js
@@ -1,8 +1,18 @@
-import dynamic from 'next/dynamic';
+import { Fragment } from "react";
+import Link from "next/link"
+import { useRouter } from "next/router"
 
-const NavMenuItem = dynamic(
-  () => import('./Nav-Menu-I'),
-  { ssr: false }
-);
+function NavMenuItem(props) {
+  const router = useRouter()
+
+  return (
+    <Link href={{pathname: `/${router.query.shopid}/${props.link}`, query: { shopid: router.query.shopid },}} style={{ textDecoration: 'none' }}>
+      <button className="navmenu-item">
+        <div className={`menu-${props.logo}`}>&nbsp;</div>
+        <h2 className="heading-secondary">{props.label}</h2>
+      </button>
+      </Link>
+  );
+}
 
 export default NavMenuItem;
