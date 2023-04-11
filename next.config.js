b2f5cc566b02038af4990f1ab589b7e21c3a60fe
JeyM <jeymson9000@gmail.com>
2023-04-11 18:50:37 +0800

    Possibly right method.

--- next.config.js
+++ next.config.js
@@ -1,19 +1,16 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  
-}
-
-module.exports = nextConfig
-
-module.exports = {
+  images: {
+    domains: ["i.imgur.com"],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
       use: ['@svgr/webpack'],
     })
-
     return config
   },
 }
+
+module.exports = nextConfig;
--- src/components/category/Category.js
+++ src/components/category/Category.js
@@ -1,6 +1,7 @@
 import Link from "next/link"
 import { useRouter } from "next/router"
 import { motion } from "framer-motion";
+import Image from "next/image";
 
 function Category(props) {
     const MotionLink = motion(Link);
@@ -14,7 +15,7 @@ function Category(props) {
 
         <button className="categ-edit-button" onClick={(event) => {props.edit2([categoryName, categoryImage, categoryDescription]); props.edit(event);}}><div className="heading-icon-edit">&nbsp;</div></button>
 
-        <img src={categoryImage} className="category-img"></img>
+        <Image src={categoryImage} className="category-img" layout="fill"></Image>
         <div className="category-content">
             <div>
                 <h2 className="heading-secondary category-name">{categoryName.length > 40 ? categoryName.substring(0, 37) + "..." : categoryName}</h2>
