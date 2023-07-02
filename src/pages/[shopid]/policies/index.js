e98b265dd780d86444e2c67d56db61b31f6e241a
JeyM <jeymson8000@gmail.com>
2023-07-02 14:57:26 +0800

    important

--- src/pages/[shopid]/policies/index.js
+++ src/pages/[shopid]/policies/index.js
@@ -4,26 +4,27 @@ import { getServerSideProps } from "../categories";
 import { useState } from "react";
 import { useRouter } from "next/router";
 import ReactMarkdown from "react-markdown";
+import gfm from "remark-gfm";
 
 function Policies(martID) {
-  const router = useRouter();
-  const [markdownContent, setMarkdownContent] = useState("");
-  const [hidden, setHidden] = useState(false);
+    const router = useRouter();
+    const [markdownContent, setMarkdownContent] = useState("");
+    const [hidden, setHidden] = useState(false);
 
-  const handleInputChange = (event) => {
-    setMarkdownContent(event.target.value);
-  };
+    const handleInputChange = (event) => {
+        setMarkdownContent(event.target.value);
+    };
 
-  const favicon = martID.shopID.shopData.shopDetails.imageData.icons.icon;
+    const favicon = martID.shopID.shopData.shopDetails.imageData.icons.icon;
 
-  return (
-    <Fragment>
-      <Head>
-        <title>Terms and Conditions</title>
-        <link rel="icon" type="image/jpeg" href={favicon} />
-      </Head>
+    return (
+        <Fragment>
+            <Head>
+                <title>Terms and Conditions</title>
+                <link rel="icon" type="image/jpeg" href={favicon} />
+            </Head>
 
-      <style jsx global>{`
+            <style jsx global>{`
         /* Use GitHub Markdown CSS globally */
         .markdown-body {
           box-sizing: border-box;
@@ -40,30 +41,32 @@ function Policies(martID) {
         }
       `}</style>
 
-      <span className="page-heading">
-        <div className="heading-icon-dropshadow">
-          <div className="heading-icon-policy svg-color">&nbsp;</div>
-        </div>
-        <h1 className="heading-primary no-margin">&nbsp;Terms and Conditions&nbsp;</h1>
-      </span>
+            <span className="page-heading">
+                <div className="heading-icon-dropshadow">
+                    <div className="heading-icon-policy svg-color">&nbsp;</div>
+                </div>
+                <h1 className="heading-primary no-margin">&nbsp;Terms and Conditions&nbsp;</h1>
+            </span>
 
-      <div className="policy-container">
-        <textarea
-          value={markdownContent}
-          onChange={handleInputChange}
-          placeholder="Enter Markdown Content"
-          className="markdown-half markdown-textarea"
-        ></textarea>
-        <div className="markdown-half markdown-preview">
-          <ReactMarkdown
-            className="markdown-body"
-            children={markdownContent}
-            skipHtml={false} // Render raw HTML within Markdown
-          />
-        </div>
-      </div>
-    </Fragment>
-  );
+            <div className="policy-container">
+                <textarea
+                    value={markdownContent}
+                    onChange={handleInputChange}
+                    placeholder="Enter Markdown Content"
+                    className="markdown-half markdown-textarea"
+                ></textarea>
+                <div className="markdown-half markdown-preview">
+                    <ReactMarkdown
+                        className="markdown-body"
+                        children={markdownContent}
+                        skipHtml={false} // Render raw HTML within Markdown
+                        remarkPlugins={[gfm]}
+                    />
+
+                </div>
+            </div>
+        </Fragment>
+    );
 }
 
 export default Policies;
