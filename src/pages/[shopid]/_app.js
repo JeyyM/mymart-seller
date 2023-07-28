904799b0e52cdb7ed5dd2c668c4a1a490e15bbfd
JeyM <jeymson8000@gmail.com>
2023-07-28 08:03:46 +0800

    Fixing up error pages

--- src/pages/[shopid]/_app.js
+++ src/pages/[shopid]/_app.js
@@ -1,6 +1,8 @@
 import "./main.scss";
 import NavbarLayout from "@/components/navbar/Navbar-Layout";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter()
   return <NavbarLayout><Component {...pageProps} /></NavbarLayout>;
 }
